f802f1282cf62cb1c16de60b02055b11056fed96
yyyyy <ipooo346@gmail.com>
2021-02-02 18:36:40 -0300

    finished ATM func (yet no refactored)

--- ATM/main.rb
+++ ATM/main.rb
@@ -21,6 +21,7 @@ def atm(value,storage)
 end
 =end
 
+=begin
 def atm(value,storage)
     change = []
     case value 
@@ -56,6 +57,43 @@ def atm(value,storage)
         "Write a valid value"
     end
 end
-
+=end
+def atm(value,storage)
+    change = []
+    while value > 0
+        puts "im here"
+        if value -100 >= 0
+            change << 100
+            value -= 100
+            puts "Your actual change is #{change}"
+            sleep 0.5
+        elsif value -50 >= 0
+            change << 50
+            value -= 50
+            puts "Your actual change is #{change}"
+            sleep 0.5
+        elsif value -20 >= 0
+            change << 20
+            value -= 20
+            puts "Your actual change is #{change}"
+            sleep 0.5
+        elsif value -10 >= 0 
+            change << 10
+            value -= 10
+            puts "Your actual change is #{change}"
+            sleep 0.5
+        elsif value -5 >= 0 
+            change << 5
+            value -= 5
+            puts "Your actual change is #{change}"
+            sleep 0.5
+        elsif value -1 >= 0
+            change << 1
+            value -= 1
+        else 
+            "Write a valid value"
+        end
+    end
+end
 
 atm(value,storage)
