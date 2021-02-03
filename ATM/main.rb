848a10484945e5b0ab2d9ba3a4c51c625a1c9dfa
yyyyy <ipooo346@gmail.com>
2021-02-03 14:24:33 -0300

    removed comments

--- ATM/main.rb
+++ ATM/main.rb
@@ -8,60 +8,9 @@ storage = {100 => 1, 50 => 2, 20 => 4, 10 => 8, 5 => 16, 1 => 32}
 
 value = welcome
 
-=begin
-def atm(value,storage)
-    change = []
-    if value - 100 > 0
-        value -= 100
-        puts value
-        change << 100
-        puts "Your change is #{change}"
-    elsif
-    end
-end
-=end
-
-=begin
-def atm(value,storage)
-    change = []
-    case value 
-    when value -100 >= 0
-        change << 100
-        value -= 100
-        puts "Your actual change is #{change}"
-        sleep 0.5
-    when  value -50 >= 0
-        change << 50
-        value -= 50
-        puts "Your actual change is #{change}"
-        sleep 0.5
-    when value -20 >= 0
-        change << 20
-        value -= 20
-        puts "Your actual change is #{change}"
-        sleep 0.5
-    when value -10 >= 0 
-        change << 10
-        value -= 10
-        puts "Your actual change is #{change}"
-        sleep 0.5
-    when value -5 >= 0 
-        change << 5
-        value -= 5
-        puts "Your actual change is #{change}"
-        sleep 0.5
-    when value -1 >= 0
-        change << 1
-        value -= 1
-    else 
-        "Write a valid value"
-    end
-end
-=end
 def atm(value,storage)
     change = []
     while value > 0
-        puts "im here"
         if value -100 >= 0
             change << 100
             value -= 100
@@ -95,5 +44,6 @@ def atm(value,storage)
         end
     end
 end
+
 
 atm(value,storage)
