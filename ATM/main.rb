f8811f49004e02bbc0ce9997800351f045d44e58
yyyyy <ipooo346@gmail.com>
2021-02-03 14:36:36 -0300

    reduced atm func and removed needless messages

--- ATM/main.rb
+++ ATM/main.rb
@@ -14,34 +14,24 @@ def atm(value,storage)
         if value -100 >= 0
             change << 100
             value -= 100
-            puts "Your actual change is #{change}"
-            sleep 0.5
         elsif value -50 >= 0
             change << 50
             value -= 50
-            puts "Your actual change is #{change}"
-            sleep 0.5
         elsif value -20 >= 0
             change << 20
             value -= 20
-            puts "Your actual change is #{change}"
-            sleep 0.5
         elsif value -10 >= 0 
             change << 10
             value -= 10
-            puts "Your actual change is #{change}"
-            sleep 0.5
         elsif value -5 >= 0 
             change << 5
             value -= 5
-            puts "Your actual change is #{change}"
-            sleep 0.5
         elsif value -1 >= 0
             change << 1
             value -= 1
-            puts "Your actual change is #{change}"
         end
     end
+    puts "Your change is #{change}"
 end
 
 
