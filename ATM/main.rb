ce8a407186130bc51d790c063b2e9deeee17ab93
yyyyy <ipooo346@gmail.com>
2021-02-08 15:53:55 -0300

    fixed loop and storage definition

--- ATM/main.rb
+++ ATM/main.rb
@@ -7,23 +7,22 @@ end
 
 #storage = {100 => 1, 50 => 2, 20 => 4, 10 => 8, 5 => 16, 1 => 32} <- (.to_a) hash to array convention
 
-loop do
-    storage = [[100, 1],[50, 2],[20, 4],[10, 8],[5, 16],[1, 32]]
+def atm(value,storage
     value = welcome(storage)
-
-    def atm(value,storage,changes)
-        change = []
-        i = 0
-        while value > 0
-            while value - storage[i][0] >= 0
-                value -= storage[i][0]
-                change << storage[i][0]
-                storage[i][1] -= 1
-            end
-            i+=1
+    change = []
+    i = 0
+    while value > 0
+        while value - storage[i][0] >= 0
+            value -= storage[i][0]
+            change << storage[i][0]
+            storage[i][1] -= 1
         end
-        puts "Your change is #{change}"
+        i+=1
     end
+    puts "Your change is #{change}"
 end
 
-atm(value,storage,changes)
+storage = [[100, 1],[50, 2],[20, 4],[10, 8],[5, 16],[1, 32]]
+loop do 
+    atm(value,storage)
+end
