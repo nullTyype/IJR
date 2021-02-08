17fb67e8d36efc695de9cdfbacda37e45a74af87
yyyyy <ipooo346@gmail.com>
2021-02-08 15:43:08 -0300

    up

--- ATM/test.rb
+++ ATM/test.rb
@@ -1,6 +1,8 @@
+#testing atm logic type convention
+
 storage = {100 => 1, 50 => 2, 20 => 4, 10 => 8, 5 => 16, 1 => 32}
 
-storage = storage.to_a
+puts storage = storage.to_a
 storage[0][1] += 1
 puts storage
 
