8008a43257d981d28c9dd9149245f46bf69f8526
yyyyy <ipooo346@gmail.com>
2021-02-08 16:04:24 -0300

    fixed current storage message and removed useless code

--- ATM/main.rb
+++ ATM/main.rb
@@ -1,16 +1,14 @@
 def welcome(storage)
     puts "Welcome, write a value to be drawed: "
     value = gets.to_i
-    puts  "Our current storage is #{storage}"
     value
 end
-
-#storage = {100 => 1, 50 => 2, 20 => 4, 10 => 8, 5 => 16, 1 => 32} <- (.to_a) hash to array convention
 
 def atm(storage)
     value = welcome(storage)
     change = []
     i = 0
+
     while value > 0
         while value - storage[i][0] >= 0
             value -= storage[i][0]
@@ -25,4 +23,5 @@ end
 storage = [[100, 1],[50, 2],[20, 4],[10, 8],[5, 16],[1, 32]]
 loop do 
     atm(storage)
+    puts "Out current storage is #{storage}"
 end
