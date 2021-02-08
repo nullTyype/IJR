081a5c74719e53b21eb7b7a0e35849a14ff45d50
yyyyy <ipooo346@gmail.com>
2021-02-08 15:46:05 -0300

    implemented ATM loop

--- ATM/main.rb
+++ ATM/main.rb
@@ -1,30 +1,30 @@
 def welcome
-    print "Welcome, write a value to be drawed: "
+    puts "Welcome, write a value to be drawed: "
+    puts  "" 
     value = gets.to_i
     value
 end
 
 #storage = {100 => 1, 50 => 2, 20 => 4, 10 => 8, 5 => 16, 1 => 32} <- (.to_a) hash to array convention
-storage = [[100, 1],[50, 2],[20, 4],[10,8],[5, 16],[1, 32]]
 
-value = welcome
+loop do
+    storage = [[100, 1],[50, 2],[20, 4],[10,8],[5, 16],[1, 32]]
+    value = welcome
 
-changes = [100,50,20,10,5,1]
-
-def atm(value,storage,changes)
-    change = []
-    i = 0
-    while value > 0
-        while value - storage[i][0] >= 0
-            value -= storage[i][0]
-            change << storage[i][0]
-            storage[i][1] -= 1
+    def atm(value,storage,changes)
+        change = []
+        i = 0
+        while value > 0
+            while value - storage[i][0] >= 0
+                value -= storage[i][0]
+                change << storage[i][0]
+                storage[i][1] -= 1
+            end
+            i+=1
         end
-        i+=1
+        puts "Your change is #{change}"
     end
-    puts "Your change is #{change}"
 end
-
 
 atm(value,storage,changes)
 puts storage.to_h
