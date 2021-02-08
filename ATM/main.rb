5ed758b23595f56a7eb3787718de8661adf94ef9
yyyyy <ipooo346@gmail.com>
2021-02-08 18:08:21 -0300

    fixed entire atm func (now working properly)

--- ATM/main.rb
+++ ATM/main.rb
@@ -10,18 +10,23 @@ def atm(storage)
     i = 0
 
     while value > 0
-        while value - storage[i][0] >= 0
-            value -= storage[i][0]
-            change << storage[i][0]
-            storage[i][1] -= 1
+        while value - storage[i][0] >= 0 && storage[i][1] > 0
+            if storage[i][1] - 1 >= 0
+                value -= storage[i][0]
+                storage[i][1] -= 1
+                change << storage[i][0]
+            else
+                puts "You exceed the max value"
+            end
         end
         i+=1
     end
-    puts "Your change is #{change}"
+    puts "Your change is #{change} \n\n"
 end
 
 storage = [[100, 1],[50, 2],[20, 4],[10, 8],[5, 16],[1, 32]]
+
 loop do 
     atm(storage)
-    puts "Out current storage is #{storage}"
+    puts "Our current storage is #{storage} \n\n"
 end
