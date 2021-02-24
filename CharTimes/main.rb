bafeed59e0d8ad0f3b9e1e21df1ca54b2de52bb7
yyyyy <ipooo346@gmail.com>
2021-02-24 20:03:21 -0300

    fixed output

--- CharTimes/main.rb
+++ CharTimes/main.rb
@@ -1,15 +1,19 @@
-char = File.read("entrada.txt")
+char = File.read("input.txt")
 
 i = 0
 k = 1
 length = char.length-1
 for i in 0..length
+    str = ""
+    File.read("output.txt") == " " ? mode= "a" : mode= "a"
+
     if char[i] == char[i+1]
-        File.write("saida.txt","#{k}#{char[i]}", mode: "a")
+        str << "#{k}#{char[i]}"
         k+=1
         i+=1
     else
-        File.write("saida.txt","#{k}#{char[i]}", mode: "a")
+        str << "#{k}#{char[i]}"
         k = 1
     end
+    File.write("output.txt","#{str}", mode: mode)
 end 
