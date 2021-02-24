29e38c5aa03bce6f2b825bc8725ca033371776e1
yyyyy <ipooo346@gmail.com>
2021-02-24 19:19:36 -0300

    setting basic logic

--- CharTimes/main.rb
+++ CharTimes/main.rb
@@ -1,11 +1,14 @@
 char = File.read("entrada.txt")
 
-
+i = 0
 k = 1
-for i in 0..char.length do
-    #k = 1
+for i in 0..char.length 
     if char[i] == char[i+1]
-        puts "#{k}#{char[i-1]}"
+        puts "#{k}#{char[i]}"
         k+=1
+        i+=1
+    elsif char[i] != char[i+1]
+        puts "#{k}#{char[i]}"
+        k = 0
     end
-end
+end 
