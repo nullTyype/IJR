d8c1dd5f777a5f502c5fc3b3ef46bb6c763de00a
yyyyy <ipooo346@gmail.com>
2021-02-24 20:09:18 -0300

    fixed output file write mode

--- CharTimes/main.rb
+++ CharTimes/main.rb
@@ -3,17 +3,17 @@ char = File.read("input.txt")
 i = 0
 k = 1
 length = char.length-1
+File.write("output.txt","", mode: "w")
 for i in 0..length
     str = ""
-    File.read("output.txt") == " " ? mode= "a" : mode= "a"
 
     if char[i] == char[i+1]
-        str << "#{k}#{char[i]}"
+        str << "#{k}#{char[i]} \s"
         k+=1
         i+=1
     else
-        str << "#{k}#{char[i]}"
+        str << "#{k}#{char[i]} \s"
         k = 1
     end
-    File.write("output.txt","#{str}", mode: mode)
+    File.write("output.txt","#{str}", mode: "a")
 end 
