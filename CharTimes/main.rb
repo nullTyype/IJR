f9008f4d9e51a74365936e4aa704a9ebee35373d
yyyyy <ipooo346@gmail.com>
2021-02-24 19:47:23 -0300

    fix file name, now output is being saved at output.txt

--- CharTimes/main.rb
+++ CharTimes/main.rb
@@ -5,11 +5,11 @@ k = 1
 length = char.length-1
 for i in 0..length
     if char[i] == char[i+1]
-        puts "#{k}#{char[i]}"
+        File.write("saida.txt","#{k}#{char[i]}", mode: "a")
         k+=1
         i+=1
     else
-        puts "#{k}#{char[i]}"
+        File.write("saida.txt","#{k}#{char[i]}", mode: "a")
         k = 1
     end
 end 
