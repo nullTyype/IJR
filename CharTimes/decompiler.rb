d2992dce70a0e5e33ebec905c3253195d835c352
yyyyy <ipooo346@gmail.com>
2021-02-25 20:13:58 -0300

    fixed decompiler dont repeating output number bug

--- CharTimes/decompiler.rb
+++ CharTimes/decompiler.rb
@@ -3,10 +3,12 @@ char = File.read("output.txt").split("\s")
 text = ""
 length = char.length-1
 for i in 0..length
-    if char[i][2]
-        text << char[i][2]
-    else
-        text << char[i][1]
+    for j in 1..char[i][0].to_i
+        if char[i][2]
+            text << char[i][2]
+        else
+            text << char[i][1]
+        end
     end
 end
 puts text
