f751ff536be9bf658808016a8e0a42afb5050fdc
yyyyy <ipooo346@gmail.com>
2021-02-25 20:18:18 -0300

    changed folder name(that one makes more sense)

---
+++ compiler-decompiler/compiler.rb
@@ -0,0 +1,29 @@
+char = File.read("input.txt")
+
+i = 0
+k = 1
+length = char.length-1
+File.write("output.txt","", mode: "w")
+for i in 0..length
+    str = ""
+
+    if char[i].match(/[[:alpha:]]/)
+        if char[i] == char[i+1]
+            k+=1
+            i+=1
+        else
+            str << "#{k}#{char[i]} \s"
+            k = 1
+        end
+    else
+        if char[i] == char[i+1]
+            k+=1
+            i+=1
+        else
+            str << "#{k}-#{char[i]} \s"
+            k = 1
+        end
+    end
+
+    File.write("output.txt","#{str}", mode: "a")
+end 
