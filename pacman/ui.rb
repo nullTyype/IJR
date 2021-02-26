b67fdfc33b6fc467b562311069e675a32300bb24
yyyyy <ipooo346@gmail.com>
2021-02-26 19:15:16 -0300

    added require_move and draw func

--- pacman/ui.rb
+++ pacman/ui.rb
@@ -1,9 +1,22 @@
 def welcome
-    puts "Welcome to Pacman"
+    puts "Welcome to Pacman's game"
     puts "Your name is? "
     name = gets.strip
     puts "\n\n\n\n\n\n"
     puts "The game is starting"
     sleep 0.5
     name
+end
+
+def draw(map)
+    puts map
+end
+
+def require_move
+    puts "Where do you want to move?"
+    move = gets.strip
+    while true
+        draw(map)
+        direction = require_move
+    end
 end
