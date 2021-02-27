b400fd7ab2d1ef716130a4f12730a2def0c37988
yyyyy <ipooo346@gmail.com>
2021-02-27 19:26:52 -0300

    refactored find_hero and added prompt commands

--- pacman/logic.rb
+++ pacman/logic.rb
@@ -6,13 +6,13 @@ def read_map(number)
     map = text.split("\n")
 end
 
-def find_player(map)
+def find_hero(map)
     hero = "H"
     map.each_with_index do |current_line, line|
         current_line = map[line]
-        hero_column = current_line[column].index(hero)
+        hero_column = current_line.index(hero)
         if hero_column != -1
-            #find
+            return [line,hero_column]
         end
     end
     #didnt find
@@ -22,7 +22,20 @@ def play(name)
     map = read_map(1)
     while true
         draw(map)
-        direction = require_move
+        direction = require_move.upcase
+        hero = find_hero(map)
+        map[hero[0]] [hero[1]] = " "
+        case direction
+            when "W"
+                hero[0] -=1
+            when "S"
+                hero[0] +=1
+            when "A"
+                hero[1] -=1
+            when "D"
+                hero[1] +=1
+        end
+        map[hero[0]][hero[1]] = "H"
     end
 end
 
