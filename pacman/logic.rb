b034e1363fdb61358cbd272375b1aa4dc32b862e
yyyyy <ipooo346@gmail.com>
2021-03-07 19:42:29 -0300

    removed unnecessary comment

--- pacman/logic.rb
+++ pacman/logic.rb
@@ -52,13 +52,6 @@ end
 def valid_pos_from(map, pos)
     positions = []
     down = [pos[0]+1,pos[1]]
-=begin
-    puts "down #{down}"
-    puts "map[0]+1 #{map[pos[0]+1][pos[1]]}"
-    puts "map[0][1] #{map[pos[0]][pos[1]]}"
-    puts "pos #{pos}"
-    puts "down[0] #{down[0]}"
-=end
     if valid_pos?(map, down)
         positions << down
     end
