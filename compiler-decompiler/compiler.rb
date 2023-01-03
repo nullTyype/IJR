b9853088a38383d7c242cc22a1182a8e16f4bfe4
Vinicius Souza <ipooo346@gmail.com>
2023-01-03 18:42:47 -0300

    iteration

--- compiler-decompiler/compiler.rb
+++ compiler-decompiler/compiler.rb
@@ -3,7 +3,9 @@ char = File.read("input.txt")
 i = 0
 k = 1
 length = char.length-1
-File.write("output.txt","", mode: "w")
+
+ File.write("output.txt","", mode: "w")
+
 for i in 0..length
     str = ""
 
